f890f04e1d964bc463166f058b8b81a752b9d943
logonrm <logonrm@L2704MICRO03.fiap.com.br>
2017-12-07 22:30:50 -0200

    Aula jaas

---
+++ fiap-aula5-seguranca/src/fiap/scj/seguranca/UsuarioEntidade.java
@@ -0,0 +1,46 @@
+package fiap.scj.seguranca;
+
+import java.security.Principal;
+
+public class UsuarioEntidade implements Principal{
+
+	private String nome;
+	private String login;
+	private String senha;
+	
+	public UsuarioEntidade(String login2) {
+		this.login = login2;
+	}
+
+	@Override
+	public String getName() {
+		return this.login;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+
+}
